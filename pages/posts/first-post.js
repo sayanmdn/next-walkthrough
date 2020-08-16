91cbaaab9c3f2400e1baa616ea6c779dc0228104
Sayantan Mishra <sayanmdn@gmail.com>
2020-08-16 11:37:59 +0530

    add link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,17 +3,12 @@ import Link from 'next/link'
 import Layout from '../../components/layout'
 
 export default function FirstPost() {
-  return (
-    <Layout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+        </Layout>
+    )
 }
