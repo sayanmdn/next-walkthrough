00464eb14db5cc0d59137a28bc1d0fb61c6dad9d
Sayantan Mishra <sayanmdn@gmail.com>
2020-08-20 22:29:24 +0530

    form  integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/global.css'
+import Head from 'next/head'
+import Link from 'next/link'
+// import Layout from '../../components/layout'
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+export default function FirstPost() {
+  return (
+    <div>
+      <div className='p-8 justify-center items-center h-screen flex'>
+        <form className='flex'>
+          <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='email' type='email' aria-label='email address' placeholder='Enter your email address' />
+          <button className='bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r' type='submit'>
+            Sign Up
+        </button>
+        </form>
+      </div>
+    </div>
+  )
+}
